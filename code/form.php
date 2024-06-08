69e0e3ec4c86ae4965055b63b2c75a4ec32f23b7
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-06-08 16:53:13 +0545

    Update: only .pdf are allowed condition is added.

--- code/form.php
+++ code/form.php
@@ -2,11 +2,19 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $target_dir = "uploads/";
     $target_location = $target_dir . basename($_FILES["document"]["name"]);
-    echo $target_location;
-    if(isset($_POST["submit"])) {
-        move_uploaded_file($_FILES["document"]["tmp_name"], $target_location);
-        print("Thank you. Your file is uploaded.");
+
+    $extension = pathinfo($_FILES["document"]["name"], PATHINFO_EXTENSION);
+    $allowed_exs = array("pdf");
+
+    if (in_array($extension, $allowed_exs)) {
+        if(isset($_POST["submit"])) {
+            move_uploaded_file($_FILES["document"]["tmp_name"], $target_location);
+            print("Thank you. Your file is uploaded.");
+        }
+    }else{
+        print($extension." file type is not allowed. File type must be .pdf only!!");
     }
+
 }
 ?>
 <form method="POST" enctype="multipart/form-data">
